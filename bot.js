977386924001fc25844dc4de0b84c6ed81b32808
Siar Wahidi <siar.wahidi@gmail.com>
2023-05-17 11:13:33 -0400

    if the message begins with bang weather then the location variable is set accordingly and passed to the getWeatherData function.

--- bot.js
+++ bot.js
@@ -12,9 +12,24 @@ const client = new Client({
   ],
 });
 
-// When teh client/bot is ready, run this code (only once)
+// When the client/bot is ready, run this code (only once)
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on("messageCreate", async (message) => {
+  // location variable is set if discrod message begins with !weather
+  if (message.content.startsWith("!weather")) {
+    const location = message.content.split(" ")[1];
+    try {
+      let response = await getWeatherData(location);
+
+      console.log(response);
+    } catch (error) {
+      console.log(`Error retrieving weather data: ${error}`);
+      message.channel.send("Sorry, I couldn't retrieve the weather data.");
+    }
+  }
 });
 
 client.login(process.env.TOKEN);
