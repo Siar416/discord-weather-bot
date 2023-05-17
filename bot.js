ed21a451f0beddbe11e1700feeee7304816de186
Siar Wahidi <siar.wahidi@gmail.com>
2023-05-17 10:57:46 -0400

    Added comments. Replaced client.on with client.once instead, and making use of the Events object

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
 require("dotenv/config");
-const { Client, IntentsBitField, EmbedBuilder } = require("discord.js");
+const { Client, IntentsBitField, EmbedBuilder, Events } = require("discord.js");
 const { getWeatherData } = require("./utils/getWeatherData");
 
+// Create a new client instance
 const client = new Client({
   intents: [
-    // Server/channel
+    // Server/channel - Guild refers to Discord server
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
   ],
 });
 
-client.on("ready", () => {
-  console.log("Weather bot is online!");
+// When teh client/bot is ready, run this code (only once)
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.login(process.env.TOKEN);
